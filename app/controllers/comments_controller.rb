87adc8a8416b1e1059aa1fe2c892d99753d041c5
mikhailGH <91605629+mikhailGH@users.noreply.github.com>
2021-09-29 16:24:59 +0300

    fixed comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,7 +11,7 @@ end
   
   def destroy
     @article = Article.find(params[:article_id])
-    @comment = @article.comments.find(params[:d])
+    @comment = @article.comments.find(params[:id])
     @comment.destroy
     redirect_to article_path(@article)
   end
